473695eeba5cf520202df1a202b927c8a82201fb
Maya-Claveau <maya.claveau@gmail.com>
2022-10-15 16:00:00 +0000

    Initial commit

---
+++ kosmo_creations/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for kosmo_creations project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kosmo_creations.settings')
+
+application = get_asgi_application()
