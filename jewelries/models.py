db9ee585af7f3abc89aff1c004332805ea595c8f
Maya-Claveau <maya.claveau@gmail.com>
2022-10-20 20:18:20 +0000

    Add customization for admin page

--- jewelries/models.py
+++ jewelries/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 
 class Category(models.Model):
+    """ model for category """
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -13,6 +18,7 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    """ model for product """
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField()
