fdc010853288f4e6a14730f5a8b2087fdb05332b
Maya-Claveau <maya.claveau@gmail.com>
2022-10-20 22:00:08 +0000

    Add content to jewelries.html

--- jewelries/views.py
+++ jewelries/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
+
 
 # Create your views here.
+def all_jewelries(request):
+    """ to render all jewelries, as well as sorting and searching functions  """
+
+    jewelries = Product.objects.all()
+
+    context = {
+        'jewelries': jewelries,
+    }
+
+    return render(request, 'jewelries/jewelries.html', context)
