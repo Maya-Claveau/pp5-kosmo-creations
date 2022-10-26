548e6e0c5810d89b289f106728c0ab554e0ae41d
Maya-Claveau <maya.claveau@gmail.com>
2022-10-26 14:03:59 +0000

    Add inventory to jewelry model and in stock lebel to shopping cart page

--- jewelries/models.py
+++ jewelries/models.py
@@ -19,12 +19,17 @@ class Category(models.Model):
 
 class Jewelry(models.Model):
     """ model for jewelries """
+
+    class Meta:
+        verbose_name_plural = 'Jewelries'
+
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    inventory = models.BooleanField(default=False, null=True, blank=True)
 
     def __str__(self):
         return self.name
