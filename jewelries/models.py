aa6dd6d8294f7e9ed37dce8aa5c8ad2289a5da0c
Maya-Claveau <maya.claveau@gmail.com>
2022-10-27 08:50:42 +0000

    modify: inventory field to integerfiled on the jewelry model

--- jewelries/models.py
+++ jewelries/models.py
@@ -29,7 +29,10 @@ class Jewelry(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    inventory = models.BooleanField(default=False, null=True, blank=True)
+    inventory = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
+
+    def has_inventory(self):
+        return self.inventory > 0
