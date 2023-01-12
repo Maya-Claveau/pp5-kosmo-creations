a39b3c4845920cae2ceb638e44021077a75280d9
Maya-Claveau <maya.claveau@gmail.com>
2023-01-12 16:55:30 +0000

    Add basic function of stripe payment

--- checkout/views.py
+++ checkout/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from shopping_cart.contexts import shopping_cart_contents
+
+import stripe
 
 
 # Create your views here.
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+
     shopping_cart = request.session.get('shopping_cart', {})
     if not shopping_cart:
         messages.error(
@@ -13,12 +21,22 @@ def checkout(request):
             )
         return redirect(reverse('jewelries'))
 
+    current_shopping_cart = shopping_cart_contents(request)
+    total = current_shopping_cart['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51Lk6uiI4fRay6ykcPgtj79S4i4LT4uW08jt4RGdiTdyZGKx6pJ3KFqG2DC1vDlijVdYU5UcFKEKx6XRhlg34vaky00DCvwl8vu',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
