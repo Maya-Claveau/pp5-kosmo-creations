eb99cac95402c4c5a1171deeec72a3db2fe2dda1
Maya-Claveau <maya.claveau@gmail.com>
2023-01-14 21:40:31 +0000

    Add forms.py to Jewelry app

---
+++ jewelries/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .widgets import CustomClearableFileInput
+
+from .models import Jewelry, Category
+
+
+class JewelryForm(forms.ModelForm):
+
+    class Meta:
+        model = Jewelry
+        fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)  # noqa
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-1'
