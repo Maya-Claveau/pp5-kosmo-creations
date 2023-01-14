181fd55649510452fa8c238e4841e28ddf373d5d
Maya-Claveau <maya.claveau@gmail.com>
2023-01-14 19:14:49 +0000

    Add profile form, views and updated template

---
+++ profiles/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_phone': 'Phone Number',
+            'default_address1': 'Address1',
+            'default_address2': 'Address2',
+            'default_city': 'City',
+            'default_county_province_state': 'County, Province or State',
+            'default_post_code': 'Post Code',
+        }
+
+        self.fields['default_phone'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'border-black rounded-1 profile-form-input'  # noqa
+            self.fields[field].label = False
--- profiles/models.py
+++ profiles/models.py
@@ -17,7 +17,8 @@ class UserProfile(models.Model):
     default_address2 = models.CharField(max_length=150, blank=True, null=True)
     default_city = models.CharField(max_length=80, blank=True, null=True)
     default_county_province_state = models.CharField(max_length=90, blank=True, null=True)  # noqa
-    default_country = CountryField(blank_label='Country *')
+    default_post_code = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(blank_label='Country')
 
     def __str__(self):
         return self.user.username
