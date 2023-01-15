0d75159cac9a253b0f1aa2d1da49261de584d02a
Maya-Claveau <maya.claveau@gmail.com>
2023-01-15 15:08:00 +0000

    Add cache control to settings file

--- kosmo_creations/settings.py
+++ kosmo_creations/settings.py
@@ -182,6 +182,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if 'USE_AWS' in os.environ:
+    # cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # bucket config
     AWS_STORAGE_BUCKET_NAME = 'kosmo-creations'
     AWS_S3_REGION_NAME = 'eu-north-1'
