c0f9ec7dd90f2ca9dfe634336c87e981ae550afd
Maya-Claveau <maya.claveau@gmail.com>
2023-03-05 19:26:56 +0000

    Add error 400 pg

--- kosmo_creations/views.py
+++ kosmo_creations/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
 
+def handler400(request, exception):
+    """ Error 400 - Bad Request """
+    return render(request, "errors/400.html", status=400)
+
+
 def handler404(request, exception):
     """ Error Handler 404 - Page Not Found """
     return render(request, "errors/404.html", status=404)
