f8fdf1ad187e209e55dcafb9c548d99f9b97c2e4
Maya-Claveau <maya.claveau@gmail.com>
2023-03-05 19:22:01 +0000

    Modify 500 error handler view

--- kosmo_creations/views.py
+++ kosmo_creations/views.py
@@ -6,6 +6,6 @@ def handler404(request, exception):
     return render(request, "errors/404.html", status=404)
 
 
-def handler500(request, exception):
+def handler500(request, *args, **argva):
     """ Error handler 500 - internal server error """
     return render(request, "errors/500.html", status=500)
