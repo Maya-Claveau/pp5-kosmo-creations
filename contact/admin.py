2a05b6c0f369aba587696c0801a253dc906b91f9
Maya-Claveau <maya.claveau@gmail.com>
2023-03-05 20:24:53 +0000

    Add admin.py to contact app

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    """
+    Admin class to display email subject and message
+    """
+    list_display = (
+        'email',
+        'subject',
+        'message'
+    )
