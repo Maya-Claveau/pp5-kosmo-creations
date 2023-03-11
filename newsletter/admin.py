31d1daa85354c22c37bf059aaf40476a82a2daa2
Maya-Claveau <maya.claveau@gmail.com>
2023-03-11 18:20:15 +0000

    Modify Newsletter model

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
-
 from .models import NewsletterSubscriber, Newsletter
 
-admin.site.register(NewsletterSubscriber)
-admin.site.register(Newsletter)
+
+class SubscriberAdmin(admin.ModelAdmin):
+    """ display NewsletterSubscriber model in Admin """
+    list_display = (
+        'email', 'date_added'
+    )
+
+    search_fields = ('email', 'date_added')
+    ordering = ('date_added',)
+
+
+class NewsletterAdmin(admin.ModelAdmin):
+    """ display newsletter model in Admin """
+    list_display = (
+        'subject', 'status', 'created', 'updated'
+    )
+
+    search_fields = ('title', 'message')
+
+
+admin.site.register(NewsletterSubscriber, SubscriberAdmin)
+admin.site.register(Newsletter, NewsletterAdmin)
--- newsletter/models.py
+++ newsletter/models.py
@@ -24,7 +24,7 @@ class Newsletter(models.Model):
     body = models.TextField()
     status = models.CharField(max_length=10, choices=EMAIL_STATUS_CHOICE)
     created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.subject
