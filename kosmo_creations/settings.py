56996e0b14345da008bbfb3d49bc017a812e3147
Maya-Claveau <maya.claveau@gmail.com>
2023-03-12 13:42:48 +0000

    Try to fix custom error page 400 500 not displaying

--- kosmo_creations/settings.py
+++ kosmo_creations/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
-# DEBUG = True
+
 
 
 ALLOWED_HOSTS = ['kosmo-creations.herokuapp.com', 'localhost']
--- kosmo_creations/views.py
+++ kosmo_creations/views.py
@@ -3,14 +3,14 @@ from django.shortcuts import render
 
 def handler400(request, exception):
     """ Error 400 - Bad Request """
-    return render(request, "errors/400.html", status=400)
+    return render(request, "errors/400.html", {})
 
 
 def handler404(request, exception):
     """ Error Handler 404 - Page Not Found """
-    return render(request, "errors/404.html", status=404)
+    return render(request, "errors/404.html", {})
 
 
 def handler500(request, *args, **argva):
     """ Error handler 500 - internal server error """
-    return render(request, "errors/500.html", status=500)
+    return render(request, "errors/500.html", {})
