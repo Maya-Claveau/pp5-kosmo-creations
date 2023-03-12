74a5dd207fd1c3b3c901f16947370816ac0de2b0
Maya-Claveau <maya.claveau@gmail.com>
2023-03-12 13:23:04 +0000

    set debug to true

--- kosmo_creations/settings.py
+++ kosmo_creations/settings.py
@@ -25,8 +25,8 @@ if os.path.isfile('env.py'):
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
-
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['kosmo-creations.herokuapp.com', 'localhost']
